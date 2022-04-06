41686ae851c4c5c5ef0673b57a3f66c9ba27803f
nadireirem <nadireulsular@gmail.com>
2022-04-06 19:45:27 -0400

    Created the loop in master

---
+++ src/test_case/Problem.java
@@ -0,0 +1,10 @@
+package test_case;
+
+public class Problem {
+    public static void main(String[] args) {
+        for (int j = 0; j <6 ; j++) {
+            System.out.println(j);
+
+        }
+    }
+}
